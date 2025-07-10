c0bc708fa5da9d8d5f40eef6f8cbce0184ac6d55
Mobit5 <91831754+Mobit5@users.noreply.github.com>
2025-07-10 19:11:22 +0300

    Update LoginSignup.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -12,7 +12,7 @@ const LoginSignup = () => {
 
   const login = async () => {
     let dataObj;
-    await fetch('http://localhost:4000/login', {
+    await fetch('https://shopper-rnec.onrender.com', {
       method: 'POST',
       headers: {
         Accept:'application/form-data',
